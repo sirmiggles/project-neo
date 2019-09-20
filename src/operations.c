72d072b9f8a0aaae0bf1ffd9615be77632a25783
Miguel A.S. Tabadero <48539976+sirmiggles@users.noreply.github.com>
2019-09-20 16:27:11 +0800

    Refactored matrix addition

--- src/operations.c
+++ src/operations.c
@@ -7,12 +7,14 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include <float.h>
 #include <omp.h>
 
 #include "matrix.h"
 
 /*  Calculate the trace of a square matrix  */
+/*  Generic case - O(n) | n = numNonZero    */
 float trace(Matrix matrix) {
     float trace = 0.0;
     int i = 0;
@@ -24,6 +26,7 @@ float trace(Matrix matrix) {
 }
 
 /*  Scalar Multiply a given matrix  */
+/*  Generic Case - O(n) | n = numNonZero  */
 void scalarMultiply(Matrix* matrix, float scalar) {
     unsigned long numElements = matrix->numCols * matrix->numRows;
     for (unsigned long i = 0; i < numElements; i++) {
@@ -32,6 +35,7 @@ void scalarMultiply(Matrix* matrix, float scalar) {
     }
 }
 
+/*  Memory address swap  */
 void swapNumbers(int* i, int* j) {
     int temp = *i;
     *i = *j;
@@ -39,6 +43,7 @@ void swapNumbers(int* i, int* j) {
 }
 
 /*  Transpose a given matrix  */
+/*  Generic case - O(n)       */
 void transpose(Matrix* matrix) {
     unsigned long numElements = matrix->numCols * matrix->numRows;
     for (unsigned long i = 0; i < numElements; i++) {
@@ -49,25 +54,76 @@ void transpose(Matrix* matrix) {
 }
 
 /*  Add two given matrices  */
+/*  Best case  -  O(n), where n = numNonZero, n(m1) == n(m2)  */
+/*  Worst case -  O(n1 + n2), n1 = m1NumNonZero and n2 = m2NumNonZero  */
 Matrix add(Matrix matrix1, Matrix matrix2) {
+    //  Initialize output matrix
     Matrix output;
     output.sourceFile = NULL;
-    output.numRows = matrix1.numRows;
-    output.numCols = matrix1.numCols;
+    output.type     = matrix1.type;
+    output.numRows  = matrix1.numRows;
+    output.numCols  = matrix1.numCols;
+    output.numNonZero = 0;
 
-    unsigned int numElements = output.numRows * output.numCols;
-    output.coo = malloc(sizeof(CoordForm)* numElements);
+    int maxSize = matrix1.numNonZero + matrix2.numNonZero;
+    output.coo = malloc(sizeof(CoordForm) * maxSize);
     if (output.coo == NULL) {
         output.type = ERR;
         return output;
     }
 
-    for (int i = 0; i < numElements; i++) {
-        output.coo[i].i = matrix1.coo[i].i;
-        output.coo[i].j = matrix1.coo[i].j;
-        output.coo[i].value = matrix1.coo[i].value + matrix2.coo[i].value;
+    //  Copy the elements of matrix 1 (technically O(n))
+    memcpy(output.coo, matrix1.coo, sizeof(CoordForm) * matrix1.numNonZero);
+    output.numNonZero = matrix1.numNonZero;
+
+    //  Before matrix 2
+    printf("Before: \n");
+    for (int i = 0; i < output.numNonZero; i++) {
+        printf("%d | %d  : %10.6f\n", output.coo[i].i, output.coo[i].j, output.coo[i].value);
+    }
+    printf("\n");
+
+    CoordForm* noMatch = malloc(sizeof(CoordForm) * matrix2.numNonZero);
+    if (noMatch == NULL) {
+        output.type = ERR;
+        return output;
+    }
+    printf("Malloc'd noMatch!\n");
+
+    int minIndex = 0;
+    int noMatchIndex = 0;
+    for (int i = 0; i < output.numNonZero; i++) {
+        for (int j = minIndex; j < matrix2.numNonZero; j++) {
+            //  Match found
+            if (output.coo[i].i == matrix2.coo[j].i && output.coo[i].j == matrix2.coo[j].j) {
+                output.coo[i].value += matrix2.coo[j].value;
+                minIndex++;
+                break;
+            }
+            //  No longer in the same row as element, so skip it from now on
+            if (output.coo[i].i > matrix2.coo[j].i) {
+                noMatch[noMatchIndex] = matrix2.coo[j];
+                noMatchIndex++;
+                minIndex++;
+            }
+        }
     }
 
+    //  Cells with no match
+    for (int i = 0; i < noMatchIndex; i++) {
+        printf("%d | %d : %10.6f\n", noMatch[i].i, noMatch[i].j, noMatch[i].value);
+    }
+
+    //  Copy noMatches
+    memcpy(output.coo + output.numNonZero, noMatch, sizeof(CoordForm) * (noMatchIndex + 1));
+    output.numNonZero += noMatchIndex;
+    free(noMatch);
+
+    printf("After: \n");
+    for (int i = 0; i < output.numNonZero; i++) {
+        printf("%d | %d  : %10.6f\n", output.coo[i].i, output.coo[i].j, output.coo[i].value);
+    }
+    printf("\n");
     return output;
 }
 
